8514e60111e344ea93b06c8f59233e66288d1189
dwarowski <realcarenthusiast@yandex.ru>
2024-10-25 22:46:34 +0300

    first part

--- ttk_bot.py
+++ ttk_bot.py
@@ -4,13 +4,11 @@ from telebot import types
 bot = telebot.TeleBot('7659124438:AAGJEiu7fVOET0Vy_hypEfdq0YZTJ25xwJI') # Токен бота
 
 
+@bot.message_handler(commands=['start'])    # Обработка начала диалога
 
+def welcome_message(message):   # вывод приветственного сообщения
 
-@bot.message_handler(content_types=['text'])    # Обработка сообщений
-
-def welcome_message(message):
-
-    keyboard = types.InlineKeyboardMarkup(); # Инициализация клавиатуры
+    keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры
     
     key_ttk_user_enter = types.InlineKeyboardButton(text='1. Войти как клиент ТТК', callback_data='enter') # Кнопка «Войти как клиент ТТК»
     
@@ -18,12 +16,35 @@ def welcome_message(message):
     
     keyboard.add(key_ttk_user_enter, key_ttk_user_conclude_contract) # Добавляем кнопки в клавиатуру
     
-    welocme_text = 'Здраствуйте, я бот помощник ТТК, если вы пользователь выберите 1. Если вы не пользуетесь услугами, пожалуйста выберите 2'
-    bot.send_message(message.from_user.id, text=welocme_text, reply_markup=keyboard)
-
-@bot.message_handler(content_types=['audio'])
-def test(message):
-    print(type(message))
+    welcome_text = 'Здраствуйте, я бот помощник ТТК, если вы пользователь выберите 1. Если вы не пользуетесь услугами, пожалуйста выберите 2' # Приветственный 
+    bot.send_message(message.from_user.id, text=welcome_text, reply_markup=keyboard) # Отправка сообщения пользователю
 
 
-bot.polling(none_stop=True, interval=0)
+@bot.callback_query_handler(func=lambda call: True) # Обработка нажатия на кнопку
+
+def callback_worker(call): # обрабатывем кнопки
+    if call.data == "enter": 
+
+        user_input_text = 'Пожалуйста введите свой номер договора, чтобы я понял кто вы' # Текст для вывода
+
+        keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры
+        key_back = types.InlineKeyboardButton(text='Назад', callback_data='back') # Кнопка «Назад»
+        keyboard.add(key_back) # Добавялем кнопку 
+
+        bot.send_message(call.from_user.id, text=user_input_text, reply_markup=keyboard) # Отправка сообщения пользователю
+
+    elif call.data == "conclude":
+        user_input_text = 'Укажите свой контактный номер и адрес для подключения услуги' # Текст для вывода
+
+        keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры
+        key_back = types.InlineKeyboardButton(text='Назад', callback_data='back') # Кнопка «Назад»
+        keyboard.add(key_back) # Добавялем кнопку 
+
+        bot.send_message(call.from_user.id, text=user_input_text, reply_markup=keyboard) # Отправка сообщения пользователю
+
+    if call.data == "back":
+        welcome_message(call) 
+
+
+
+bot.polling(none_stop=True)
