d2d89739ee7c997fc2026cf833d5a607bc046c89
dwarowski <realcarenthusiast@yandex.ru>
2024-10-26 21:21:37 +0300

    contract validation

--- ttk_bot.py
+++ ttk_bot.py
@@ -1,24 +1,25 @@
 import telebot
 import requests
 import re
-from telebot import types 
+from telebot import types
 
-
-bot_token ='7659124438:AAGJEiu7fVOET0Vy_hypEfdq0YZTJ25xwJI' # Токен бота
+bot_token = '7659124438:AAGJEiu7fVOET0Vy_hypEfdq0YZTJ25xwJI' # Токен бота
 
 bot = telebot.TeleBot(bot_token) # Объект тг-бот
 
-welcome_text = f'Здраствуйте, я бот-помощник ТТК, Чтобы войти напишите "Войти как клиент ТТК". Если хотите заключить новый догвор, пожалуйста напишите "Заключить новый договор"' 
-user_contract_enter_text = f'Пожалуйста введите свой номер договора, чтобы я понял кто вы' 
-user_contact_info_text = f'Укажите свой контактный номер и адрес для подключения услуги' 
-bot_not_understand_text = f'Извини, я тебя не понял. Давай еще раз'
-user_wrong = f'Неправильно, попробуйте еще раз'
+welcome_text = 'Здраствуйте, я бот-помощник ТТК, Чтобы войти напишите "Войти как клиент ТТК". Если хотите заключить новый догвор, пожалуйста напишите "Заключить новый договор"' 
+user_contract_enter_text = 'Пожалуйста введите свой номер договора, чтобы я понял кто вы' 
+user_contact_info_text = 'Укажите свой контактный номер и адрес для подключения услуги' 
+bot_not_understand_text = 'Извини, я тебя не понял. Давай еще раз'
+user_wrong = 'Неправильно, попробуйте еще раз'
+
 
 def back_to_start_keyboard(): # Клавиатура возврата в начало
     keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры в один ряд
     key_back = types.InlineKeyboardButton(text='Назад', callback_data='back_to_start') # Кнопка «Назад»
     keyboard.add(key_back) # Добавялем кнопку 
     return keyboard
+
 
 def welcome_keyboard(): # Клавиатура приветственная
     keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры в один ряд
@@ -27,17 +28,20 @@ def welcome_keyboard(): # Клавиатура приветственная
     keyboard.add(key_ttk_user_enter, key_ttk_user_conclude_contract) # Добавляем кнопки в клавиатуру
     return keyboard
 
+
 def voice_message_download(message): # Сохранение аудио
     file_info = bot.get_file(message.voice.file_id)
     file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(bot_token, file_info.file_path))
     with open(f'{message.from_user.id}.ogg','wb') as f:
         f.write(file.content)
 
+
 def next_step_and_output_message(message, user_output_text, keyboard, next_func): # Конструктор отправки сообщения и следущего шага
     bot.send_message(message.from_user.id, text=user_output_text, reply_markup=keyboard) # Отправка сообщения пользователю
     bot.register_next_step_handler(message, next_func) # Указание следующий функции
 
-phone_pattern = re.compile(r"(\+\d{1,3})?\s?\(?\d{1,4}\)?[\s.-]?\d{3}[\s.-]?\d{4}")
+
+phone_pattern = re.compile(r"(\+\d{1,3})?\s?\(?\d{1,4}\)?[\s.-]?\d{3}[\s.-]?\d{4}") # Паттерн телефона
 ''' 
 "+1 (555) 123-4567"
 "555-123-4567"
@@ -45,22 +49,25 @@ phone_pattern = re.compile(r"(\+\d{1,3})?\s?\(?\d{1,4}\)?[\s.-]?\d{3}[\s.-]?\d{4
 "+44 (0) 20 1234 5678"
 "02012345678"
 '''
-def validate_phone_number(regex, phone_number):
+def validate_phone_number(regex, phone_number): # Проверка на номер телефона в строке
     match = re.search(regex, phone_number)
     if match:
         return True
     return False
 
+
 @bot.message_handler(commands=['start'])    # Обработка начала диалога
 def welcome_message_output(message):   # Вывод приветственного сообщения
     bot.send_message(message.from_user.id, text=welcome_text, reply_markup=welcome_keyboard()) # Отправка сообщения пользователю
+
 
 @bot.message_handler(content_types=['voice'])   # Обработка голосовых
 def start_voice_message(message):
     voice_message_download(message)
 
-@bot.message_handler(content_types=['text'])  
-def start_text_message(message):
+
+@bot.message_handler(content_types=['text'])  # Обработка текстовых 
+def start_text_message(message): # Начальное сообщение 
     if message.text in ['1', 'Войти как клиент ТТК', 'Войти', 'войти']:
         next_step_and_output_message(message, user_contract_enter_text, back_to_start_keyboard(), enter_as_client)
         
@@ -70,32 +77,34 @@ def start_text_message(message):
     else: 
         next_step_and_output_message(message, bot_not_understand_text, None, start_text_message)
 
-def enter_as_client(message):
-    print(message.text)
-    if message.text.isdigit() and len(message.text) == 9 and message.text[:6] == '516':
-        print('succses_enter')
 
-    elif message.text.lower() == 'назад':
+def enter_as_client(message): # Вход как клиент ТТК
+    print(message.text)
+    if re.search(r'[516]+[0-9]{6}', message.text):
+        print('succses_enter') 
+
+    elif message.text.lower() == 'назад': # Возврат назад 
         next_step_and_output_message(message, welcome_text, welcome_keyboard(), start_text_message)
 
-    else:
+    else: # Неправильный ввод и повтор ввода 
         next_step_and_output_message(message, user_wrong, back_to_start_keyboard(), enter_as_client)
         
-def conclude_contract(message): 
 
-    if validate_phone_number(phone_pattern, message.text):
-        phone_number = re.search(phone_pattern, message.text)[0]
-        adress = message.text.replace(phone_number, '', 1)
-        adress_filtered = "".join(c for c in adress if c.isalpha())
+def conclude_contract(message): # Заключение нового контракта
+
+    if validate_phone_number(phone_pattern, message.text): # Валидация
+        phone_number = re.search(phone_pattern, message.text)[0] # Находим номер в строке
+        adress = message.text.replace(phone_number, '', 1) # Убираем из текста номер
+        adress_filtered = "".join(c for c in adress if c.isalpha()) # Убираем ненужные символы 
 
         print(phone_number)
         print(adress_filtered)
 
-    elif message.text.lower() == 'назад':
-        next_step_and_output_message(message, welcome_text, welcome_keyboard(), start_text_message)
+    elif message.text.lower() == 'назад': # Возврат назад 
+        next_step_and_output_message(message, welcome_text, welcome_keyboard(), start_text_message) #
 
-    else:
-        next_step_and_output_message(message, user_wrong, back_to_start_keyboard(), conclude_contract)
+    else: # Неправильный ввод и повтор ввода 
+        next_step_and_output_message(message, user_wrong, back_to_start_keyboard(), conclude_contract) #
 
 
 
