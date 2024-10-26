40c649e7c24f6a7a73caec1129ab261260d1e644
Tretchian <safonovila7@gmail.com>
2024-10-26 12:14:40 +0300

    Mail sender class

--- mailSender.py
+++ mailSender.py
@@ -1,19 +1,43 @@
 import smtplib
+import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-sender_email = "test@example.com"
-receiver_email = "receiver@example.com"
-subject = "This message was sent as test"
-body = "Test body of the message will be here"
+class mailSender():
+    # Настройки отправителя и получателя
+    sender_email = "safonovila7@gmail.com"
+    receiver_email = "safonovila7@gmail.com"
+    app_password = os.getenv('EMAIL_PASS')
 
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = subject
-message.attach(MIMEText(body, "plain"))
+    def __init__(self,sender_address:str,reciever_address:str):
+        self.sender_email = sender_address
+        self.receiver_email = reciever_address
+        self.message = MIMEMultipart()
 
-with smtplib.SMTP("localhost", 1025) as server:
-    server.sendmail(sender_email, receiver_email, message.as_string())
+    def set_sender_reciever(self,sender_address:str,reciever_address:str):
+        self.sender_email = sender_address
+        self.receiver_email = reciever_address
 
-print("Тестовое письмо отправлено.")
+    def set_account_password(self,password:str):
+        self.app_password = password
+
+    def send_email(self):
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+                server.login(self.sender_email, self.app_password)
+                server.sendmail(self.sender_email, self.receiver_email, self.message.as_string())
+            print("Письмо успешно отправлено.")
+        except Exception as e:
+            print(f"Ошибка: {e}")
+
+    def create_message(self,subject:str,body:str) -> MIMEMultipart :
+        # Записывет сообщение в свой объект и возвращает его же
+        self.message["From"] = self.sender_email
+        self.message["To"] = self.receiver_email
+        self.message["Subject"] = subject
+        self.message.attach(MIMEText(body, "plain"))
+        return self.message
+
+ms = mailSender("safonovila7@gmail.com","safonovila7@gmail.com")
+ms.create_message("Субъект","Суть сообщения тут")
+ms.send_email()
