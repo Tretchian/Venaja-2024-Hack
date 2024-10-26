4046a95ddc7e4309df0a29c433192aa4a2607264
Tretchian <safonovila7@gmail.com>
2024-10-26 16:14:39 +0300

    Added receiver list

--- mailSender.py
+++ mailSender.py
@@ -2,6 +2,7 @@ import smtplib
 import os
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from multipledispatch import dispatch
 
 
 class mailSender():
@@ -11,14 +12,12 @@ class mailSender():
     receiver_list = []
     app_password = ""
 
-    def __init__(self, sender_address: str, reciever_address: str):
+    def __init__(self, sender_address: str):
         self.sender_email = sender_address
-        self.receiver_email = reciever_address
         self.message = MIMEMultipart()
 
-    def set_sender_reciever(self, sender_address: str, reciever_address: str):
+    def set_sender(self, sender_address: str):
         self.sender_email = sender_address
-        self.receiver_email = reciever_address
 
     def set_account_password(self, password: str):
         self.app_password = password
@@ -34,6 +33,33 @@ class mailSender():
         except Exception as e:
             print(f"Ошибка: {e}")
 
+    @dispatch(list)
+    def send_email_tolist(self, reciever_list: list):
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+                server.login(self.sender_email, self.app_password)
+                for reciever in reciever_list:
+                    server.sendmail(self.sender_email,
+                                    reciever,
+                                    self.message.as_string())
+            print("Письма успешно отправлены.")
+        except Exception as e:
+            print(f"Ошибка: {e}")
+
+    @dispatch()
+    def send_email_tolist(self):
+        try:
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
+                server.login(self.sender_email, self.app_password)
+                for reciever in self.receiver_list:
+                    server.sendmail(self.sender_email,
+                                    reciever,
+                                    self.message.as_string())
+            print("Письма успешно отправлены.")
+        except Exception as e:
+            print(f"Ошибка: {e}")
+
+    @dispatch(str, str)
     def create_message(self, subject: str, body: str) -> MIMEMultipart:
         # Записывет сообщение в свой объект и возвращает его же
         self.message["From"] = self.sender_email
@@ -41,7 +67,8 @@ class mailSender():
         self.message["Subject"] = subject
         self.message.attach(MIMEText(body, "plain"))
         return self.message
-    
+
+    @dispatch(list)
     def create_message(self, contents: list) -> MIMEMultipart:
         # Записывет сообщение в свой объект и возвращает его же
         self.message["From"] = self.sender_email
@@ -49,20 +76,26 @@ class mailSender():
         self.message["Subject"] = contents[0]
         self.message.attach(MIMEText(contents[1], "plain"))
         return self.message
-    
-    def add_reciever(self,new_receiver_addres:str):
-        if not new_receiver_addres: return
-        if self.receiver_list.__contains__(new_receiver_addres): return
-        
 
+    def add_reciever(self, new_receiver_address: str):
+        if not new_receiver_address: return
+        if new_receiver_address in self.receiver_list:
+            print(f'{new_receiver_address} is in list already')
+            return
+
+        self.receiver_list.append(new_receiver_address)
+
+    def del_reciever(self, receiver_address: str):
+        if not receiver_address: return
+        if receiver_address not in self.receiver_list:
+            print(f'{receiver_address} is not in list already')
+            return
+        self.receiver_list.remove(receiver_address)
 
 '''Пример использования класса
-ms = mailSender("safonovila7@gmail.com","safonovila7@gmail.com")
-ms.create_message("Субъект","Суть сообщения тут")
+ms = mailSender("safonovila7@gmail.com")
 ms.set_account_password(os.getenv('EMAIL_PASS'))
-ms.send_email()
+ms.create_message(['тема','текст'])
+
+ms.send_email_tolist(["safonovila7@gmail.com","fe-ct@mail.ru","maxbek294@gmail.com"])
 '''
-ms = mailSender("safonovila7@gmail.com","safonovila7@gmail.com")
-ms.create_message(['Тема','текст'])
-ms.set_account_password(os.getenv('EMAIL_PASS'))
-ms.send_email()
