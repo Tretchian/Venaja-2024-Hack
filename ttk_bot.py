5133c97f7b94a07b168a4f86a51c145c9f4fa8c1
dwarowski <realcarenthusiast@yandex.ru>
2024-10-26 12:16:44 +0300

    barely working task

--- ttk_bot.py
+++ ttk_bot.py
@@ -1,61 +1,83 @@
 import telebot
+import requests
 from telebot import types 
 
-bot = telebot.TeleBot('7659124438:AAGJEiu7fVOET0Vy_hypEfdq0YZTJ25xwJI') # Токен бота
+bot_token ='7659124438:AAGJEiu7fVOET0Vy_hypEfdq0YZTJ25xwJI'
 
-def data_sender():
+bot = telebot.TeleBot(bot_token) # Токен бота
+
+
+def client_data_sender():
      @bot.message_handler(content_types=['text'])    # Обработка ввода
      def send_data(message):
         print(message.text, "1")
 
-@bot.message_handler(commands=['start'])    # Обработка начала диалога
+def contract_data_sender():
+    @bot.message_handler(content_types=['text'])    # Обработка ввода
+    def send_data(message):
+        print(message.text, "2")
+    
+def back_to_start():
+    keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры в один ряд
+    key_back = types.InlineKeyboardButton(text='Назад', callback_data='back_to_start') # Кнопка «Назад»
+    keyboard.add(key_back) # Добавялем кнопку 
+    return keyboard
 
-def welcome_message(message):   # Вывод приветственного сообщения
-
-    keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры
-    
-    key_ttk_user_enter = types.InlineKeyboardButton(text='1. Войти как клиент ТТК', callback_data='enter') # Кнопка «Войти как клиент ТТК»
-    
-    key_ttk_user_conclude_contract= types.InlineKeyboardButton(text='2. Заключить новый договор', callback_data='conclude') # Кнопка «Заключить новый договор»
-    
+def welcome_keyboard():
+    keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры в один ряд
+    key_ttk_user_enter = types.InlineKeyboardButton(text='1. Войти как клиент ТТК', callback_data='enter_as_client') # Кнопка «Войти как клиент ТТК»
+    key_ttk_user_conclude_contract= types.InlineKeyboardButton(text='2. Заключить новый договор', callback_data='conclude_contract') # Кнопка «Заключить новый договор»
     keyboard.add(key_ttk_user_enter, key_ttk_user_conclude_contract) # Добавляем кнопки в клавиатуру
+    return keyboard
+
+def voice_message_download(message):
+    file_info = bot.get_file(message.voice.file_id)
+    file = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(bot_token, file_info.file_path))
+    with open(f'{message.from_user.username}_{message.from_user.id}.ogg','wb') as f:
+        f.write(file.content)
+
+def text_(message):
+    print(message.text)
+
+
+@bot.message_handler(commands=['start'])    # Обработка начала диалога
+def welcome_message_output(message):   # Вывод приветственного сообщения
+
+    welcome_text = 'Здраствуйте, я бот помощник ТТК, если вы пользователь ТТК выберите 1. Если вы не пользуетесь услугами, пожалуйста выберите 2' # Приветственный 
     
-    welcome_text = 'Здраствуйте, я бот помощник ТТК, если вы пользователь выберите 1. Если вы не пользуетесь услугами, пожалуйста выберите 2' # Приветственный 
-    bot.send_message(message.from_user.id, text=welcome_text, reply_markup=keyboard) # Отправка сообщения пользователю
+    bot.send_message(message.from_user.id, text=welcome_text, reply_markup=welcome_keyboard()) # Отправка сообщения пользователю
+
+
+@bot.message_handler(content_types=['text','voice'])  
+def first_message(message):
+    if message.voice:
+        voice_message_download(message)
+    if message.text:
+         text_
+        
 
 
 @bot.callback_query_handler(func=lambda call: True) # Обработка нажатия на кнопку
-
 def callback_worker(call): # обрабатывем кнопки
-    if call.data == "enter": # Если пользователь зарегистрирован заходим сюда
+    if call.data == "enter_as_client": # Если пользователь зарегистрирован заходим сюда
 
-        user_input_text = 'Пожалуйста введите свой номер договора, чтобы я понял кто вы' # Текст для вывода
+        user_output_text = 'Пожалуйста введите свой номер договора, чтобы я понял кто вы' # Текст для вывода
 
-        keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры
-        key_back = types.InlineKeyboardButton(text='Назад', callback_data='back') # Кнопка «Назад»
-        keyboard.add(key_back) # Добавялем кнопку 
-
-        bot.send_message(call.from_user.id, text=user_input_text, reply_markup=keyboard) # Отправка сообщения пользователю
+        bot.send_message(call.from_user.id, text=user_output_text, reply_markup=back_to_start()) # Отправка сообщения пользователю
                 
-        data_sender()
+        client_data_sender()
        
 
-    elif call.data == "conclude": # Если хочет заключить договор 
-        user_input_text = 'Укажите свой контактный номер и адрес для подключения услуги' # Текст для вывода
+    elif call.data == "conclude_contract": # Если хочет заключить договор 
+        
+        user_output_text = 'Укажите свой контактный номер и адрес для подключения услуги' # Текст для вывода
 
-        keyboard = types.InlineKeyboardMarkup(row_width=1); # Инициализация клавиатуры
-        key_back = types.InlineKeyboardButton(text='Назад', callback_data='back') # Кнопка «Назад»
-        keyboard.add(key_back) # Добавялем кнопку 
+        bot.send_message(call.from_user.id, text=user_output_text, reply_markup=back_to_start()) # Отправка сообщения пользователю
 
-        bot.send_message(call.from_user.id, text=user_input_text, reply_markup=keyboard) # Отправка сообщения пользователю
-
-        @bot.message_handler(content_types=['text'])    # Обработка ввода
-
-        def send_data(message):
-            print(message.text, "2")
-    
-    if call.data == "back": # Возврат обратно
-        welcome_message(call) 
+        contract_data_sender()
+        
+    elif call.data == "back_to_start": # Возврат обратно
+        welcome_message_output(call) 
 
 bot.polling(none_stop=True)
 
