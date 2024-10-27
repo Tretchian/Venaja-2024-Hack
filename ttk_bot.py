eec47bd8fd899e6cef69ca68596d7a3a0de7e561
dwarowski <realcarenthusiast@yandex.ru>
2024-10-27 13:53:08 +0300

    cleaned voice handler

--- ttk_bot.py
+++ ttk_bot.py
@@ -202,12 +202,6 @@ def welcome_message_output(message):
                      reply_markup=keyboard_welcoming())
 
 
-# Обработка голосовых
-@bot.message_handler(content_types=['voice'])
-def start_voice_message(message):
-    voice_message_download(message)
-
-
 # Обработка текстовых
 @bot.message_handler(content_types=['text'])
 # Начальное сообщение
